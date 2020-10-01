062eebe85fdda1ae46d69a7a81ab6ef1f7664eb4
tjbigelow <tjbigelow@att.net>
2020-10-01 13:07:44 -0500

    Fixed bugs from merge conflicts

--- lib/cabinet.rb
+++ lib/cabinet.rb
@@ -58,17 +58,10 @@ class Cabinet
             puts "That ingredient doesn't exist in our database, would you like to add it? (y/n)"
             user_input = gets.chomp.downcase
             if ['y','yes'].any?(user_input)
-<<<<<<< HEAD
-                new_ingredient = CommandLineInterface.new.add_ingredient(ingredient_input)
-                lookup_id = new_ingredient.id
-            else
-                self.build####needs to return to the building of the cabinet while retaining the cabinet from before
-=======
                 new_ingredient = Ingredient.add_ingredient(ingredient_input)
                 lookup_id = new_ingredient.id
             else
                 build####needs to return to the building of the cabinet while retaining the cabinet from before
->>>>>>> tom
             end
         else
             lookup_id = lookup_id.first
